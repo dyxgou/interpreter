0d78a8ad9f571a030df40cf7d1631dd0022f892a
dyxgou <diego5614yt@gmail.com>
2024-11-14 18:13:10 -0500

    feat : Monkey programming language lexer

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -1,43 +1,151 @@
 package lexer
 
-import "gihub.com/dyxgou/parser/src/token"
+import (
+	"strings"
+
+	"gihub.com/dyxgou/parser/src/token"
+)
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	chr          byte
+	position     int // Points to the position of the last read
+	readPosition int // Points to the reading position
+	ch           byte
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
+	l := &Lexer{
+		input:        input,
+		position:     0,
+		readPosition: 0,
+	}
 
+	l.readChar()
 	return l
 }
 
-// Modifies the chr field with the current char in the string.
-// If the the readPosition is greater than the input, the chr becomes 0.
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
-		l.chr = 0
+		l.ch = byte(token.EOF)
 	} else {
-		l.chr = l.input[l.readPosition]
+		l.ch = l.input[l.readPosition]
 	}
 
 	l.position = l.readPosition
 	l.readPosition++
 }
 
-func (l *Lexer) NextToken() {
-	var tok token.Token
+func isLetter(ch byte) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_'
+}
 
-	switch l.chr {
-	case '=':
-		tok = token.New(token.ASSIGN, string(l.chr))
-	case ';':
-		tok = token.New(token.SEMI, string(l.chr))
-	case ',':
-		tok = token.New(token.COMMA, string(l.chr))
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\r' || l.ch == '\n' {
+		l.readChar()
 	}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	default:
+		if isLetter(l.ch) {
+			t.Literal = l.readIdentifier()
+			t.Kind = token.LookupIdent(t.Literal)
+			return t
+		} else if isDigit(l.ch) {
+			t.Literal = l.readNumber()
+			t.Kind = token.INT
+			return t
+		} else {
+			t = token.New(token.ILLEGAL, string(l.ch))
+		}
+	case '=':
+		if ch := l.peekChar(); ch == '=' {
+			t = token.New(token.EQUAL, getCompositeString(l.ch, ch))
+			l.readChar()
+			break
+		}
+		t = token.New(token.ASSIGN, string(l.ch))
+	case '{':
+		t = token.New(token.LBRACE, string(l.ch))
+	case '}':
+		t = token.New(token.RBRACE, string(l.ch))
+	case '(':
+		t = token.New(token.LPAREN, string(l.ch))
+	case ')':
+		t = token.New(token.RPAREN, string(l.ch))
+	case ',':
+		t = token.New(token.COMMA, string(l.ch))
+	case ';':
+		t = token.New(token.SEMI, string(l.ch))
+	case '+':
+		t = token.New(token.PLUS, string(l.ch))
+	case '-':
+		t = token.New(token.MINUS, string(l.ch))
+	case '*':
+		t = token.New(token.MULTIPLICATION, string(l.ch))
+	case '/':
+		t = token.New(token.DIVISION, string(l.ch))
+	case '<':
+		t = token.New(token.LESS, string(l.ch))
+	case '>':
+		t = token.New(token.GREATER, string(l.ch))
+	case '"':
+		t = token.New(token.QOUTE, string(l.ch))
+	case '!':
+		if ch := l.peekChar(); ch == '=' {
+			t = token.New(token.NOT_EQUAL, getCompositeString(l.ch, ch))
+			l.readChar()
+			break
+		}
+		t = token.New(token.NOT, string(l.ch))
+	case byte(token.EOF):
+		t = token.New(token.EOF, "")
+	}
+
+	l.readChar()
+	return t
+}
+
+func (l *Lexer) readIdentifier() string {
+	pos := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	pos := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return byte(token.EOF)
+	}
+
+	return l.input[l.readPosition]
+}
+
+func getCompositeString(b ...byte) string {
+	var s strings.Builder
+
+	s.Write(b)
+
+	return s.String()
 }
--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -6,21 +6,102 @@ import (
 	"gihub.com/dyxgou/parser/src/token"
 )
 
-func TestNextToken(t *testing.T) {
-	input := "={}()[],;"
+func TestReadPosition(t *testing.T) {
+	input := "=(){}"
 
+	l := New(input)
+
+	for l.ch != byte(token.EOF) {
+		l.readChar()
+	}
+}
+
+func TestNextToken(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+!-/*+5;
+5 < 10 > 5;
+
+if (7 < 10) {
+  return true;
+} else {
+  return false;
+}
+`
 	tests := []struct {
-		expextedKind    token.TokenKind
+		expectedKind    token.TokenKind
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMI, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMI, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
 		{token.SEMI, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMI, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMI, ";"},
+		{token.NOT, "!"},
+		{token.MINUS, "-"},
+		{token.DIVISION, "/"},
+		{token.MULTIPLICATION, "*"},
+		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMI, ";"},
+		{token.INT, "5"},
+		{token.LESS, "<"},
+		{token.INT, "10"},
+		{token.GREATER, ">"},
+		{token.INT, "5"},
+		{token.SEMI, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "7"},
+		{token.LESS, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMI, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMI, ";"},
+		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
 
@@ -29,11 +110,48 @@ func TestNextToken(t *testing.T) {
 	for i, tt := range tests {
 		tok := l.NextToken()
 
-		if tok.Kind != tt.expextedKind {
+		if tok.Kind != tt.expectedKind {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expextedKind, tok.Type)
-
+				i, tt.expectedKind, tok.Kind)
 		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestDoubleToken(t *testing.T) {
+	input := `10 == 10; 
+  9 != 10;`
+
+	tests := []struct {
+		expectedKind    token.TokenKind
+		expectedLiteral string
+	}{
+		{expectedKind: token.INT, expectedLiteral: "10"},
+		{expectedKind: token.EQUAL, expectedLiteral: "=="},
+		{expectedKind: token.INT, expectedLiteral: "10"},
+		{expectedKind: token.SEMI, expectedLiteral: ";"},
+		{expectedKind: token.INT, expectedLiteral: "9"},
+		{expectedKind: token.NOT_EQUAL, expectedLiteral: "!="},
+		{expectedKind: token.INT, expectedLiteral: "10"},
+		{expectedKind: token.SEMI, expectedLiteral: ";"},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		t.Log(tok)
+
+		if tok.Kind != tt.expectedKind {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%d, got=%d",
+
+				i, tt.expectedKind, tok.Kind)
+		}
+
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 				i, tt.expectedLiteral, tok.Literal)
