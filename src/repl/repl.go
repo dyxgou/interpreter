1d53df3a50701ba962d39b62cfff7a023e207f59
dyxgou <diego5614yt@gmail.com>
2024-11-14 18:12:39 -0500

    feat: ReadEvalPrintLoop integrated

---
+++ src/repl/repl.go
@@ -0,0 +1,33 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"gihub.com/dyxgou/parser/src/lexer"
+	"gihub.com/dyxgou/parser/src/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		text := scanner.Text()
+
+		l := lexer.New(text)
+
+		for tok := l.NextToken(); tok.Kind != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%v\n", tok)
+		}
+	}
+}
