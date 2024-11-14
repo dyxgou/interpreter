0f97d0138fc0aaa02f1be20b8d78e7f6be2007e5
dyxgou <diego5614yt@gmail.com>
2024-11-14 18:13:22 -0500

    feat: Initialazing the tokenizer

--- src/main.go
+++ src/main.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/user"
+
+	"gihub.com/dyxgou/parser/src/repl"
 )
 
 func main() {
-	b, err := os.ReadFile("./examples/00.lang")
+	user, err := user.Current()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(b))
+	fmt.Printf("Hello %s! This is the Monkey Parser\n", user.Username)
+	fmt.Println("Feel free to type in the commands")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
