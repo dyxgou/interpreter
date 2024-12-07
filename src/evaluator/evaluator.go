e3482aa21d5b83efda26a357be3d4e72777f56c8
dyxgou <diego5614yt@gmail.com>
2024-12-07 11:53:08 -0500

    feat: First part of evaluator
    It evals numeric operations and boolean operations

---
+++ src/evaluator/evaluator.go
@@ -0,0 +1,154 @@
+package evaluator
+
+import (
+	"gihub.com/dyxgou/parser/src/ast"
+	"gihub.com/dyxgou/parser/src/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+const (
+	notOperator          string = "!"
+	plusOperator         string = "+"
+	minusOperator        string = "-"
+	productoOperator     string = "*"
+	divitionOperator     string = "/"
+	greaterOperator      string = ">"
+	lessOperator         string = "<"
+	greaterEqualOperator string = ">="
+	lessEqualOperator    string = "<="
+	equalOperator        string = "=="
+	notEqualOperator     string = "!="
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator(), right)
+	case *ast.InfixExpression:
+		right := Eval(node.Right)
+		left := Eval(node.Left)
+		return evalInfixExpression(node.Operator(), right, left)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{
+			Value: node.Value,
+		}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+
+	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	if right == nil {
+		return NULL
+	}
+
+	switch operator {
+	case notOperator:
+		return evalNotOperator(right)
+	case minusOperator:
+		return evalMinusOperatorExpression(right)
+	}
+
+	return NULL
+}
+
+func evalNotOperator(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	}
+
+	return FALSE
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.IntegerType {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, right, left object.Object) object.Object {
+	if right == nil || left == nil {
+		return NULL
+	}
+
+	switch {
+	case right.Type() == object.IntegerType && left.Type() == object.IntegerType:
+		return evalIntegerInfixExpression(operator, right, left)
+	case operator == equalOperator:
+		return nativeBoolToBooleanObject(left == right)
+	case operator == notEqualOperator:
+		return nativeBoolToBooleanObject(left != right)
+	}
+
+	return NULL
+}
+
+func nativeBoolToBooleanObject(input bool) object.Object {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
+}
+
+func evalIntegerInfixExpression(operator string, right, left object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case plusOperator:
+		return &object.Integer{Value: leftVal + rightVal}
+	case minusOperator:
+		return &object.Integer{Value: leftVal - rightVal}
+	case productoOperator:
+		return &object.Integer{Value: leftVal * rightVal}
+	case divitionOperator:
+		return &object.Integer{Value: leftVal / rightVal}
+	case equalOperator:
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case greaterOperator:
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case greaterEqualOperator:
+		return nativeBoolToBooleanObject(leftVal >= rightVal)
+	case lessOperator:
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case lessEqualOperator:
+		return nativeBoolToBooleanObject(leftVal <= rightVal)
+	case notEqualOperator:
+		return nativeBoolToBooleanObject(leftVal != rightVal)
+	}
+
+	return NULL
+}
---
+++ src/evaluator/evaluator_helper_test.go
@@ -0,0 +1,50 @@
+package evaluator
+
+import (
+	"testing"
+
+	"gihub.com/dyxgou/parser/src/lexer"
+	"gihub.com/dyxgou/parser/src/object"
+	"gihub.com/dyxgou/parser/src/parser"
+)
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	intObj, ok := obj.(*object.Integer)
+
+	if !ok {
+		t.Errorf("the obj is not an *object.Integer. got=%T", obj)
+		return false
+	}
+
+	if intObj.Value != expected {
+		t.Errorf("int.Value expected=%d. got=%d", expected, intObj.Value)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	boolObj, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("obj expected=*object.Boolean. got=%T", obj)
+		return false
+	}
+
+	if boolObj.Value != expected {
+		t.Errorf("boolObj valued expected=%t. got=%t", expected, boolObj.Value)
+		return false
+	}
+
+	return true
+}
